30c6bfdf0f79f5af4ede706c232fff793ac3d66e
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-07 21:10:31 +0530

    update: enhance README and apply TailwindCSS styles to page layout

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ```
  npm i --save-dev eslint-config-standard eslint-plugin-tailwindcss eslint-config-prettier prettier
 ```
+
+## Setup TailwindCSS
+
+```
+npm i --save-dev tailwindcss-animate
+```
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1> Home </h1>
+    <div className='flex-center h-screen'>
+      <h1 className='h1'> Home </h1>
     </div>
   );
 }
